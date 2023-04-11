abd166c9a3de6d155d9d6f7cc416899a6d777180
Jane <evgeniiatsyp13@gmail.com>
2023-04-11 09:33:30 +0200

    importing/exporting components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const me = {
+    'person': 'kisa',
+    'age': 25
+  };
+  const welcome = "welcome to my blog";
   return (
     <div className="App">
+      <Navbar/>
       <div className='content'>
-        <h1>App component</h1>
+        <Home />
       </div>
+
     </div>
   );
 }
