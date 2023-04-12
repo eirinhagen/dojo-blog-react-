c149bc2b4a48274d03b8103bc0488d480b06314f
Jane <evgeniiatsyp13@gmail.com>
2023-04-12 11:17:39 +0200

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,10 @@
 const Home = () => {
+
+    const handleClick = (name) => { console.log('hei '+ name)}
     return ( 
         <div className="home">
             <h1>Homepage</h1>
+            <button onClick={()=>handleClick('kisa')}>Click</button>
             
         </div>
      );
